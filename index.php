908f023ce0664ff9aaba53422740e145aa8ab89f
Amália Coelho <amalia@4.events>
2024-10-06 17:17:58 -0300

    Liberar conexão fora da porta da api

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
     // Descomente para debugar erros
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    // ini_set('display_errors', 1);
+    // ini_set('display_startup_errors', 1);
+    // error_reporting(E_ALL);
 
     require_once __DIR__."/vendor/autoload.php";
     require_once __DIR__ .'/app/config/init.php';
     
     header("Content-Type: application/json");
-    
+    header('Access-Control-Allow-Origin: *');
+
     // Captura a URL e o método da requisição
     $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $requestMethod = $_SERVER['REQUEST_METHOD'];
-    
+
     $result = $router->run($requestUri, $requestMethod);
 
     if ($result){
